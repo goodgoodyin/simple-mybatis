0cf57692625dce2c400b4957d9c1d9a007ab869e
yinhao <986487287@qq.com>
2022-12-06 08:01:21 +0800

    feat:注册器的映射和使用

---
+++ src/test/java/com/goodyin/mybatis/test/ApiTest.java
@@ -0,0 +1,27 @@
+package com.goodyin.mybatis.test;
+
+import com.goodyin.mybatis.binding.MapperProxyFactory;
+import com.goodyin.mybatis.test.dao.IUserDao;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiTest {
+
+    private Logger logger = LoggerFactory.getLogger(ApiTest.class);
+
+    @Test
+    public void test_MapperProxyFactory() {
+        MapperProxyFactory<IUserDao> factory = new MapperProxyFactory<>(IUserDao.class);
+        Map<String, String> sqlSession = new HashMap<>();
+        sqlSession.put("com.goodyin.mybatis.test.dao.IUserDao.queryUserName", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户姓名");
+        sqlSession.put("com.goodyin.mybatis.test.dao.IUserDao.queryUserAge", "模拟执行 Mapper.xml 中 SQL 语句的操作：查询用户年龄");
+        IUserDao userDao = factory.newInstance(sqlSession);
+        userDao.toString();
+        String res = userDao.queryUserAge("10001");
+        logger.info("测试结果：{}", res);
+    }
+}
---
+++ src/test/java/com/goodyin/mybatis/test/dao/IUserDao.java
@@ -0,0 +1,11 @@
+package com.goodyin.mybatis.test.dao;
+
+
+public interface IUserDao {
+
+    String queryUserName(String uId);
+
+    String queryUserAge(String uId);
+
+
+}
